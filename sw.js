d61c06c5380ee1a1d51db0c361cb0d79ed5881b8
zeixx1 <theveninyoan@gmail.com>
2026-05-15 17:54:30 +0200

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = 'ledctrl-v5';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
